403c7d3d32a592417f5e7c7155fe94ee3cebf288
Daniyar <danik.2700@gmail.com>
2022-11-15 01:11:29 +0600

    first com

---
+++ index.js
@@ -0,0 +1,22 @@
+
+let table = document.getElementById('age-table')
+
+
+table.getElementsByTagName('label')
+
+
+
+table.getElementsByTagName('td')[0]
+
+
+
+let form = document.getElementsByName('search')[0]
+
+
+
+form.getElementsByTagName('input')[0]
+
+
+
+let inputs = form.querySelectorAll('input')
+inputs[inputs.length-1] 
